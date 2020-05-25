be2eab7dad5434fa138a63647ea8275fab4fc54e
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-05-25 12:16:07 +0100

    small refactory

--- portscanner/portscan.go
+++ portscanner/portscan.go
@@ -1,22 +1,32 @@
 package main
+
 import (
 	"flag"
 	"fmt"
 	"log"
 	"net"
 )
+
 func main() {
-	host := flag.String("h", "", "-h host")
-	port := flag.Int("p", 0, "-p port")
-	flag.Parse()
-	if *host == "" || *port <= 0 {
-		panic("invalid input")
-	}
-	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", *host, *port))
+	host, port := getArgs()
+
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host, port))
 	if err != nil {
-		log.Printf("%d CLOSED (%s)\n", *port, err)
+		log.Printf("%d CLOSED (%s)\n", port, err)
 		return
 	}
 	conn.Close()
-	log.Printf("%d OPEN\n", *port)
+	log.Printf("%d OPEN\n", port)
+}
+
+func getArgs() (string, int) {
+	host := flag.String("h", "", "-h host")
+	port := flag.Int("p", 0, "-p port")
+	flag.Parse()
+
+	if *host == "" || *port <= 0 {
+		panic("invalid input")
+	}
+
+	return *host, *port
 }
